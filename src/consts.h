75707eac00a17e70a7ae522cd63122ee6c36221f
Omar <omar_bazaraa@hotmail.com>
2019-04-26 03:29:32 +0200

    Add utility class

--- src/consts.h
+++ src/consts.h
@@ -2,36 +2,47 @@
 #define __CONSTS_H_
 
 /**
- * Enum holding all the possible operators in out language.
+ * Enum holding all the possible operators in our language.
  */
-enum Operations {
-    ASSIGN = 100,       // a = b
-    PLUS,               // a + b
-    UNARY_PLUS,         // +54
-    MINUS,              // a - b
-    UNARY_MINUS,        // -54
-    MULTIPLY,           // a * b
-    DIVIDE,             // a / b
-    MOD,                // a % b
-    PREFIX_INC,         // ++a
-    SUFFIX_INC,         // a++
-    PREFIX_DEC,         // --a
-    SUFFIX_DEC,         // a--
-    AND,                // a & b
-    OR,                 // a | b
-    XOR,                // a ^ b
-    NOT,                // ~a
-    SHIFT_LEFT,         // a << 1
-    SHIFT_RIGHT,        // a >> 1
-    LOGICAL_AND,        // a && b
-    LOGICAL_OR,         // a || b
-    LOGICAL_NOT,        // !a
-    GREATER,            // a > b
-    GREATER_EQUAL,      // a >= b
-    LESS,               // a < b
-    LESS_EQUAL,         // a <= b
-    EQUAL,              // a == b
-    NOT_EQUAL,          // a != b
+enum Operator {
+    OPR_ASSIGN = 100,       // a = b
+    OPR_ADD,                // a + b
+    OPR_U_PLUS,             // +54
+    OPR_SUB,                // a - b
+    OPR_U_MINUS,            // -54
+    OPR_MUL,                // a * b
+    OPR_DIV,                // a / b
+    OPR_MOD,                // a % b
+    OPR_PRE_INC,            // ++a
+    OPR_SUF_INC,            // a++
+    OPR_PRE_DEC,            // --a
+    OPR_SUF_DEC,            // a--
+    OPR_AND,                // a & b
+    OPR_OR,                 // a | b
+    OPR_XOR,                // a ^ b
+    OPR_NOT,                // ~a
+    OPR_SHL,                // a << 1
+    OPR_SHR,                // a >> 1
+    OPR_LOGICAL_AND,        // a && b
+    OPR_LOGICAL_OR,         // a || b
+    OPR_LOGICAL_NOT,        // !a
+    OPR_GREATER,            // a > b
+    OPR_GREATER_EQUAL,      // a >= b
+    OPR_LESS,               // a < b
+    OPR_LESS_EQUAL,         // a <= b
+    OPR_EQUAL,              // a == b
+    OPR_NOT_EQUAL,          // a != b
+};
+
+/**
+ * Enum holding all the possible data types in our language.
+ */
+enum DataType {
+    DTYPE_INT = 300,
+    DTYPE_FLOAT,
+    DTYPE_CHAR,
+    DTYPE_BOOL,
+    DTYPE_VOID
 };
 
 #endif
---
+++ src/utils.h
@@ -0,0 +1,102 @@
+#ifndef __UTILS_H_
+#define __UTILS_H_
+
+#include <string>
+
+#include "consts.h"
+
+using namespace std;
+
+
+struct Utils {
+
+    /**
+     * Converts the given operator into its corresponding token string.
+     * 
+     * @param opr the operator to convert.
+     * 
+     * @return the corresponding token string.
+     */
+    static string oprToStr(Operator opr) {
+        switch (opr) {
+            case OPR_ASSIGN:
+                return "=";
+            case OPR_ADD:
+            case OPR_U_PLUS:
+                return "+";
+            case OPR_SUB:
+            case OPR_U_MINUS:
+                return "-";
+            case OPR_MUL:
+                return "*";
+            case OPR_DIV:
+                return "/";
+            case OPR_MOD:
+                return "%";
+            case OPR_PRE_INC:
+            case OPR_SUF_INC:
+                return "++";
+            case OPR_PRE_DEC:
+            case OPR_SUF_DEC:
+                return "--";
+            case OPR_AND:
+                return "&";
+            case OPR_OR:
+                return "|";
+            case OPR_XOR:
+                return "^";
+            case OPR_NOT:
+                return "~";
+            case OPR_SHL:
+                return "<<";
+            case OPR_SHR:
+                return ">>";
+            case OPR_LOGICAL_AND:
+                return "&&";
+            case OPR_LOGICAL_OR:
+                return "||";
+            case OPR_LOGICAL_NOT:
+                return "!";
+            case OPR_GREATER:
+                return ">";
+            case OPR_GREATER_EQUAL:
+                return ">=";
+            case OPR_LESS:
+                return "<";
+            case OPR_LESS_EQUAL:
+                return "<=";
+            case OPR_EQUAL:
+                return "==";
+            case OPR_NOT_EQUAL:
+                return "!=";
+        }
+
+        return "#";
+    }
+
+    /**
+     * Converts the given operator into its corresponding token string.
+     * 
+     * @param opr the operator to convert.
+     * 
+     * @return the corresponding token string.
+     */
+    static string dtypeToStr(DataType type) {
+        switch (type) {
+            case DTYPE_INT:
+                return "int";
+            case DTYPE_FLOAT:
+                return "float";
+            case DTYPE_CHAR:
+                return "char";
+            case DTYPE_BOOL:
+                return "bool";
+            case DTYPE_VOID:
+                return "void";
+        }
+
+        return "unknown";
+    }
+};
+
+#endif
