8bcc1b19afad0885340ad68618d44595423a4bfa
Omar <omar_bazaraa@hotmail.com>
2019-04-28 20:02:17 +0200

    Add initial symbol table

---
+++ src/symbol_table/symbol_table.h
@@ -0,0 +1,19 @@
+#ifndef __SYMBOL_TABLE_H_
+#define __SYMBOL_TABLE_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+
+/**
+ * Struct holding the information of the symbol table.
+ */
+struct SymbolTable {
+
+};
+
+#endif
